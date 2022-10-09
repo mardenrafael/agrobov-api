f32c9b4a8a321cf429d9a2ff2f987a695d28d647
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-09 13:46:23 -0500

    Documentação atualizada

--- src/app.ts
+++ src/app.ts
@@ -10,15 +10,27 @@ export default class App {
     this.routes();
   }
 
+  /**
+   * Configura o comportamento padrão do servidor
+   */
   private middleware(): void {
     this.app.use(json());
   }
 
+  /**
+   * Configura as rotas que vão ser usadas pelo servidor.
+   */
   private routes(): void {}
 
-  public start(port: number, host: string) {
+  /**
+   *Inicia o servidor
+   * @param port Porta que vai ser usada
+   * @param host URL que vai ser usada, nesse caso localhost
+   */
+  public start(port: number, host: string = "0.0.0.0") {
     this.app.listen(port, host, () => {
       console.log(`Server running on: ${host}:${port}`);
     });
   }
 }
+
