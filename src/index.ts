920f676aeca829b23252f13174f28862375955e8
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-10 20:56:01 -0500

    Refac: Server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import "dotenv/config";
+import express from "express";
 import App from "./app";
 
 const PORT = parseInt(process.env.PORT!);
 
-const server = new App();
-
-server.start(PORT);
+const server = new App(express(), PORT, "0.0.0.0");
 
