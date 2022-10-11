4684b59b1b06de86ecf1478ed5a9534ede486f8d
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-10 20:56:11 -0500

    refac: App refact

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,40 @@
-import express, { Express, json } from "express";
+import { Express, json } from "express";
 
 export default class App {
   private readonly app: Express;
 
-  constructor() {
-    this.app = express();
+  constructor(express: Express, port: number, host: string) {
+    this.app = express;
 
-    this.middleware();
+    this.config();
+    this.midleware();
     this.routes();
+    this.start(port, host);
   }
 
   /**
    * Configura o comportamento padrão do servidor
    */
-  private middleware(): void {
+  private config(): void {
     this.app.use(json());
   }
 
   /**
+   * Configura as rotas de midlewares
+   *
+   * @example midleware() {
+   *  // midRoute é uma rota de proteção que so permite a passagem para usuarios autorizados
+   *  this.app.use(midRoute)
+   * }
+   */
+  private midleware(): void {}
+
+  /**
    * Configura as rotas que vão ser usadas pelo servidor.
+   *
+   * @example routes() {
+   *  this.app.use(UserRoute)
+   * }
    */
   private routes(): void {}
 
@@ -27,7 +43,7 @@ export default class App {
    * @param port Porta que vai ser usada
    * @param host URL que vai ser usada, nesse caso localhost
    */
-  public start(port: number, host: string = "0.0.0.0") {
+  private start(port: number, host: string = "0.0.0.0") {
     this.app.listen(port, host, () => {
       console.log(`Server running on: ${host}:${port}`);
     });
