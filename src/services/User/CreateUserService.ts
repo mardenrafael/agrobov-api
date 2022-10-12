5a8b1b84cbd57830adac3dc44e68afffebf4bd34
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-12 16:08:46 -0500

    user password save in hash

--- src/services/User/CreateUserService.ts
+++ src/services/User/CreateUserService.ts
@@ -2,6 +2,7 @@ import { PrismaClient, User } from "@prisma/client";
 import { RequestUser } from ".";
 import { UserService } from "./interface/UserService";
 import { OmitUserRequest } from "./types/UserTypes";
+import bcrypt from "bcrypt";
 
 export default class CreateUserService implements UserService {
   private readonly prisma: PrismaClient;
@@ -15,12 +16,14 @@ export default class CreateUserService implements UserService {
     name,
     passWord,
   }: RequestUser): Promise<OmitUserRequest<User> | Error> {
+    const hashPassword = await bcrypt.hash(passWord, 10);
+
     const result: User | Error = await this.prisma.user
       .create({
         data: {
           name,
           email,
-          password: passWord,
+          password: hashPassword,
         },
       })
       .catch((err) => {
