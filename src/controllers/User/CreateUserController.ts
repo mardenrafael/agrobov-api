d49317b852af40c8cff0f3f49ff7dfef4247d5f1
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-12 00:36:06 -0500

    Create user controller Create

---
+++ src/controllers/User/CreateUserController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { CreateUserService } from "../../services/User";
+import prisma from "../../utils/Prisma";
+import UserController from "./interface/UserController";
+
+export default class CreateUserController implements UserController {
+  public async handle(req: Request, res: Response): Promise<void> {
+    const { name, email, password } = req.body;
+    const service = new CreateUserService(prisma);
+    const result = await service.execute({
+      name,
+      email,
+      password,
+    });
+
+    if (result instanceof Error) {
+      res.status(400).json({
+        error: result.name,
+        message: result.message,
+      });
+      return;
+    }
+
+    res.status(200).json({
+      result,
+    });
+  }
+}
+
