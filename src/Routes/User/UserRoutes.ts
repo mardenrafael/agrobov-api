6feab60888516d1778ba6fde8eca8d223098491f
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-12 00:36:25 -0500

    User Routes

---
+++ src/Routes/User/UserRoutes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import CreateUserController from "../../controllers/User/CreateUserController";
+import GetUserByIdController from "../../controllers/User/GetUserByIdController";
+
+export default class UserRoutes {
+  private readonly router: Router;
+
+  constructor() {
+    this.router = Router();
+
+    this.config();
+  }
+
+  private config(): void {
+    this.router.get("/user", new GetUserByIdController().handle);
+    this.router.post("/user", new CreateUserController().handle);
+  }
+
+  public export(): Router {
+    return this.router;
+  }
+}
+
