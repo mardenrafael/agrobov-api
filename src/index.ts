8af44b4459171ae5e30c4fc8dd4fda6f9b9b0f49
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-12 20:09:06 -0500

    Update index for use env var

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import App from "./app";
 
-const PORT = parseInt(process.env.PORT!);
+const PORT = parseInt(process.env.PORT!) || 3030;
 
 const server = new App(express(), PORT);
 
