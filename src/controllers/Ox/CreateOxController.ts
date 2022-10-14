215b56ddaa71bc0bc1fa5bcd842375500483efc3
magnojunior07 <magnojunior319@gmail.com>
2022-10-14 16:53:59 -0400

    add create ox controller

---
+++ src/controllers/Ox/CreateOxController.ts
@@ -0,0 +1,22 @@
+import { CreateOxService } from './../../services/Ox/CreateOxService';
+import { Request, Response } from 'express';
+import prisma from '../../utils/Prisma';
+import OxController from './interface/OxController';
+
+export default class CreateOxController implements OxController{
+  public async handle(req: Request, res: Response): Promise<void> {
+    const ownerId = req.params.id
+    const { earring, born_date} = req.body;
+    const service = new CreateOxService(prisma);
+    const result = service.execute({earring, born_date}, Number(ownerId));
+
+    if(result instanceof Error){
+      res.status(400).json({
+        error: result.message
+      });
+      return
+    };
+
+    res.status(200).json(result);
+  }
+}
