18102fd87e080f70d3ae2730acb60e00f55c2ab5
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-15 17:20:17 -0500

    pretier

--- src/services/Ox/GetOxService.ts
+++ src/services/Ox/GetOxService.ts
@@ -5,20 +5,20 @@ export class GetOxService implements OxService {
   private readonly prisma: PrismaClient;
 
   constructor(prisma: PrismaClient) {
-    this.prisma = prisma
+    this.prisma = prisma;
   }
 
-  public async execute(owner_id: number): Promise< Array<Ox> | Error> {
+  public async execute(owner_id: number): Promise<Array<Ox> | Error> {
     const result: Array<Ox> = await this.prisma.ox.findMany({
       where: {
-        ownerId: owner_id
-      }
+        ownerId: owner_id,
+      },
     });
 
     if (!result) {
-      return new Error("Ox not found!")
+      return new Error("Ox not found!");
     }
 
-    return result
+    return result;
   }
 }
