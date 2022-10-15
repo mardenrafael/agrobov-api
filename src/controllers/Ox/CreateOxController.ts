e2e2ce479ea06d5d6c2d68799fe5cf15b41f2beb
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-14 22:16:54 -0500

    fix/create Ox

--- src/controllers/Ox/CreateOxController.ts
+++ src/controllers/Ox/CreateOxController.ts
@@ -1,21 +1,24 @@
-import { CreateOxService } from './../../services/Ox/CreateOxService';
-import { Request, Response } from 'express';
-import prisma from '../../utils/Prisma';
-import OxController from './interface/OxController';
+import { CreateOxService } from "./../../services/Ox/CreateOxService";
+import { Request, Response } from "express";
+import prisma from "../../utils/Prisma";
+import OxController from "./interface/OxController";
 
-export default class CreateOxController implements OxController{
+export default class CreateOxController implements OxController {
   public async handle(req: Request, res: Response): Promise<void> {
-    const ownerId = req.params.id
-    const { earring, born_date} = req.body;
+    const ownerId = req.params.id;
+    const { earring, born_date, genre } = req.body;
     const service = new CreateOxService(prisma);
-    const result = service.execute({earring, born_date}, Number(ownerId));
+    const result = service.execute(
+      { earring, born_date, genre },
+      Number(ownerId)
+    );
 
-    if(result instanceof Error){
+    if (result instanceof Error) {
       res.status(400).json({
-        error: result.message
+        error: result.message,
       });
-      return
-    };
+      return;
+    }
 
     res.status(200).json(result);
   }
