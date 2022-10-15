1579c239dd5d7c1c4e681991234f2731ff1d9f44
magnojunior07 <magnojunior319@gmail.com>
2022-10-15 16:30:26 -0400

    add validate login service on ox routes

--- src/Routes/Ox/OxRoutes.ts
+++ src/Routes/Ox/OxRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"; 
 import GetOxController from "../../controllers/Ox/GetOxController";
 import CreateOxController from "../../controllers/Ox/CreateOxController";
+import { ValidateLogin } from "../../services/auth/ValidateLoginService";
 
 export default class OxRoutes {
   private readonly router: Router;
@@ -12,7 +13,7 @@ export default class OxRoutes {
   }
 
   private config(): void {
-    this.router.get("/user/:id/ox", new GetOxController().handle);
+    this.router.get("/user/:id/ox", new ValidateLogin().validate, new GetOxController().handle);
     this.router.post("/user/:id/ox", new CreateOxController().handle);
   }
 
