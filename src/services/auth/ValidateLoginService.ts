5497bac28f9d0aa3434acf2383b2805561e63739
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-15 18:24:07 -0500

    Update ValidateLogin

--- src/services/auth/ValidateLoginService.ts
+++ src/services/auth/ValidateLoginService.ts
@@ -2,29 +2,42 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export class ValidateLogin {
-  public async validate(req: Request, res: Response, next: NextFunction): Promise<Error | NextFunction | void> {
+  public async validate(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Error | NextFunction | void> {
     const token = req.headers.authorization;
-    
-    if(!token) {
+
+    if (!token) {
       res.status(401).json({
-        Error: "Unhautorized"
-      })
-      return
+        Error: "No Token provide!",
+      });
+      return;
     }
 
-    try {
+    const hasBearer = token?.includes("Bearer");
 
-      jwt.verify(token, process.env.JWT_SECRET!)
-      
-      next()
-
-    } catch(err){
-      
+    if (!hasBearer) {
       res.status(401).json({
-        Error: "Unhautorized"
-      })
+        Error: "Invalid token",
+      });
 
-      return
+      return;
+    }
+
+    const JWTtoken = token.split("Bearer ")[1];
+
+    try {
+      jwt.verify(JWTtoken, process.env.JWT_SECRET!) as any;
+
+      next();
+    } catch (err) {
+      res.status(401).json({
+        Error: "Unhautorized",
+      });
+
+      return;
     }
   }
 }
