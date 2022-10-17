7ef926898f7af19ee85d19efda4150e8feeaa035
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-16 20:17:00 -0500

    Create setCors middleware

---
+++ src/middlewares/CorsMiddleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import corsConfig from "../utils/Cors";
+
+export class CorsMiddleware {
+  public async setCors(
+    _req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    res.set(corsConfig);
+    next();
+  }
+}
+
