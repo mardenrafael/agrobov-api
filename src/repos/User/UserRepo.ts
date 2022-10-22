e1911badb06b3b31c2e467de4a8c0bdb022133df
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-22 13:09:58 -0500

    created update user

--- src/repos/User/UserRepo.ts
+++ src/repos/User/UserRepo.ts
@@ -8,6 +8,34 @@ export default class UserRepo implements IUser {
   constructor(prisma: PrismaClient) {
     this.prisma = prisma;
   }
+  public async updateUser({
+    name,
+    id,
+  }: Partial<Pick<TUser, "id" | "name">>): Promise<
+    Omit<TUser, "id" | "password"> | Error
+  > {
+    try {
+      const user = await this.prisma.user.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+        },
+        select: {
+          Ox: true,
+          brand: true,
+          email: true,
+          name: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
   getUserById({ id }: TUser): Promise<TUser> {
     throw new Error("Method not implemented." + id);
   }
