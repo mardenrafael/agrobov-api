562a6f33fc4ec672f52864af3ae3374e9e1acd2b
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-28 19:23:40 -0500

    updateUser service

--- src/services/User/UpdateUserService.ts
+++ src/services/User/UpdateUserService.ts
@@ -1,8 +1,7 @@
-import { UserService } from "./interface/UserService";
 import { IUser } from "../../repos/User/interfaces/IUser";
 import { TUser } from "../../repos/User/types/TUser";
 
-export default class UpdateUserService implements UserService {
+export default class UpdateUserService {
   private readonly repo: IUser;
 
   constructor(repo: IUser) {
@@ -11,13 +10,14 @@ export default class UpdateUserService implements UserService {
 
   public async execute({
     name,
-    id
-  }: Pick<TUser, "id" | "name">): Promise<Omit<TUser, "id" | "password"> | Error> {
-    
+    id,
+  }: Pick<TUser, "id" | "name">): Promise<
+    Omit<TUser, "id" | "password"> | Error
+  > {
     try {
       const user = await this.repo.updateUser({
         name,
-        id
+        id,
       });
 
       return user;
