6cfe6e383bc96540fab39134d223a0a2e10dac5a
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-10-28 19:23:27 -0500

    removed user interface

--- src/services/User/interface/UserService.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "@prisma/client";
-import { OmitUserRequest } from "../types/UserTypes";
-
-export interface UserService {
-  execute({}): Promise<OmitUserRequest<User> | Error>;
-}
