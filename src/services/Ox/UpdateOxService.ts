0bf171e5ff31144f54dd13258fdf64f99cded2a0
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-11-02 10:32:40 -0500

    update output

--- src/services/Ox/UpdateOxService.ts
+++ src/services/Ox/UpdateOxService.ts
@@ -15,7 +15,7 @@ export default class UpdateOxServiceS {
     marked,
     genre,
     born_date,
-  }: TOx): Promise<Omit<TOx, "id"> | Error> {
+  }: TOx): Promise<TOx | Error> {
     try {
       const ox = await this.repo.updateOx(
         { id },
