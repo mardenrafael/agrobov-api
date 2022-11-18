03190941ac94dd7ae1fbdb497a104e3558586846
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-11-17 20:18:50 -0500

    Update import and usnig cors Middleware

--- src/routes/auth/AuthRoutes.ts
+++ src/routes/auth/AuthRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { LoginController } from "../../controllers";
+import { CorsMiddleware } from "../../middlewares";
 
 export class Auth {
   private readonly route: Router;
@@ -11,7 +12,11 @@ export class Auth {
   }
 
   private config() {
-    this.route.post("/login", new LoginController().handle);
+    this.route.post(
+      "/login",
+      new CorsMiddleware().setCors,
+      new LoginController().handle
+    );
   }
 
   public export(): Router {
