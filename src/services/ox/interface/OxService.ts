669a4459814f566ff49e7ca5cde3e0d4d41146e4
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-11-17 19:43:22 -0500

    removed OxService interface

--- src/services/ox/interface/OxService.ts
+++
@@ -1,8 +0,0 @@
-import { TOx } from "../../../repos/ox/types/TOx";
-
-export interface OxService {
-  execute(
-    {},
-    ownerId: number
-  ): Promise<Array<Omit<TOx, "id">> | Omit<TOx, "id"> | Error>;
-}
