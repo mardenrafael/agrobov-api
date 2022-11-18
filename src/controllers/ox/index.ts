8cc12dde4ee98965f00e3d92b01c1cb835851bc6
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-11-17 20:00:30 -0500

    create index file to Ox controller

---
+++ src/controllers/ox/index.ts
@@ -0,0 +1,11 @@
+import CreateOxController from "./CreateOxController";
+import DeleteOxController from "./DeleteOxController";
+import GetOxByIdController from "./GetOxByIdController";
+import UpdateOxController from "./UpdateOxController";
+
+export {
+  CreateOxController,
+  DeleteOxController,
+  GetOxByIdController,
+  UpdateOxController,
+};
